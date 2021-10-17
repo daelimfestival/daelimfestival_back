d275c9ae74591b43b5a4928c5d9024092ee5f1d6
SeoEunGi0802 <lolove08@naver.com>
2021-10-17 21:25:55 +0900

    1017 commit
    닉네임을 삭제하고 좋아요 기능을 삭제하기로 프론트엔드와 협의로 관련 함수, 변수 삭제

--- action/board/guest_book_data.php
+++ action/board/guest_book_data.php
@@ -8,26 +8,6 @@ $token = clean_xss_tags(htmlspecialchars(trim($json->token)), 1);
 // pagination
 $page = clean_xss_tags(htmlspecialchars(trim($json->page)), 1);
 
-if (is_token($token) && alive_token($token)) {
-    $log = getDeviceData("token", $token);
-
-    if ($log) {
-        $member = getMember($log["member_idx"]);
-
-        $like_list = array();
-
-        $like_list_sql = sql_query("SELECT idx, target_idx FROM DF_like WHERE member_idx = {$log["member_idx"]}");
-
-        for ($i = 0; $res = sql_fetch_array($like_list_sql); $i++) {
-            $like_list_data = array(
-                "idx" => $res['idx']
-            );
-
-            array_push($like_list, $like_list_data);
-        }
-    }
-}
-
 if (!$page) {
     $page = 1;
 }
@@ -40,13 +20,12 @@ $response = "ok";
 
 $sql = "SELECT COUNT(idx) AS cnt FROM DF_guest_book WHERE is_delete = 'N';";
 
-$guest_book_list_sql = sql_query("SELECT DFg.idx, DFg.content, DFg.like_num, DFg.write_date FROM DF_guest_book AS DFg INNER JOIN DF_member AS DFm ON DFg.member_idx = DFm.member_idx WHERE DFg.is_delete = 'N' ORDER BY DFg.idx DESC LIMIT $limit, 25;");
+$guest_book_list_sql = sql_query("SELECT DFg.idx, DFg.content, DFg.write_date FROM DF_guest_book AS DFg INNER JOIN DF_member AS DFm ON DFg.member_idx = DFm.member_idx WHERE DFg.is_delete = 'N' ORDER BY DFg.idx DESC LIMIT $limit, 25;");
 
 for ($i = 0; $res = sql_fetch_array($guest_book_list_sql); $i++) {
     $guest_book_list_data = array(
         "idx" => $res['idx'],
         "content" => $res["content"],
-        "like_num" => $res["like_num"],
         "write_date" => viewYMD_dot($res["write_date"])
     );
 
@@ -57,7 +36,6 @@ $total = sql_fetch("SELECT COUNT(idx) AS cnt FROM DF_guest_book WHERE is_delete 
 
 $result = array(
     "response" => $response,
-    "user_like" => $like_list,
     "total" => $total,
     "list" => $guest_book_list
 );
